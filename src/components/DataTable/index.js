8a6c183ca1fa84194369a2132c387a60c5ed0b76
ayoshida <a.yoshida@xearts.jp>
2020-10-23 18:57:25 +0900

    add key property

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { Table } from "antd";
 
-const DataTable = ({data}) => {
+const DataTable = ({ data }) => {
     const columns = [
         {
             title: "Id",
             dataIndex: "Id",
+            key: "id"
         },
         {
             title: "Title",
-            dataIndex: "Title"
+            dataIndex: "Title",
+            key: "title"
         },
         {
             title: "Content",
-            dataIndex: "Content"
+            dataIndex: "Content",
+            key: "content"
         }
     ]
-    
+
     return (
-       <Table dataSource = {data} columns = {columns}/>
+        <Table dataSource={data} columns={columns} />
     );
 }
 
